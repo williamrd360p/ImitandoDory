097283e362c83b302b2d7424f66ab935cf6fcbe4
williamrd360p <willian.silva579@etec.sp.gov.br>
2024-09-27 19:55:57 -0300

    programa procurando dory
    dory

---
+++ Program.cs.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Imitando dory");
+string frase = Console.ReadLine()!;
+
+string FraseEmBaleies = frase
+
+ .Replace("a", "aaa" )
+ .Replace("A", "AAA" )
+ .Replace("e", "eee" )
+ .Replace("E", "EEE" )
+ .Replace("i", "iii" )
+ .Replace("I", "III" )
+ .Replace("o", "ooo" )
+ .Replace("O", "OOO" )
+ .Replace("u", "uuu")
+ .Replace("U", "UUU")
+;
+
+Console.WriteLine($"\n em baleiês:\n\n {FraseEmBaleies}");
+
+
+
